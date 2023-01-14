3dd2dfb0f755fda7496b98e0034506a7ac2b1e8e
bkleiner <b.kleiner@str8labs.com>
2023-01-14 02:57:18 +0100

    xradio: re-add sunxi-wlan support

---
+++ drivers/net/wireless/xradio/platform.c
@@ -0,0 +1,80 @@
+/*
+ * platform interfaces for XRadio drivers
+ *
+ * Copyright (c) 2013, XRadio
+ * Author: XRadio
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <linux/version.h>
+#include <linux/module.h>
+#include <linux/err.h>
+#include <linux/clk.h>
+#include <linux/delay.h>
+#include <linux/interrupt.h>
+#include <linux/gpio.h>
+#include <linux/ioport.h>
+#include <linux/regulator/consumer.h>
+#include <linux/sunxi-gpio.h>
+#include <linux/gpio.h>
+#include <linux/types.h>
+#include <linux/power/aw_pm.h>
+
+#include "xradio.h"
+#include "platform.h"
+#include "sdio.h"
+
+extern void sunxi_wlan_set_power(bool on);
+extern int sunxi_wlan_get_bus_index(void);
+extern int sunxi_wlan_get_oob_irq(void);
+extern int sunxi_wlan_get_oob_irq_flags(void);
+
+int xradio_wlan_power(int on)
+{
+	sunxi_wlan_set_power(on);
+	mdelay(100);
+	return 0;
+}
+
+void xradio_sdio_detect(int enable)
+{
+	int wlan_bus_id = sunxi_wlan_get_bus_index();
+	MCI_RESCAN_CARD(wlan_bus_id);
+	xr_printk(XRADIO_DBG_ALWY, "%s SDIO card %d\n", enable ? "Detect" : "Remove", wlan_bus_id);
+	mdelay(10);
+}
+
+int xradio_request_gpio_irq(struct sdio_func *func, irq_handler_t handler)
+{
+	struct device *dev = &func->dev;
+	int ret = -1;
+
+	int irq = sunxi_wlan_get_oob_irq();
+	if (!irq) {
+		xr_printk(XRADIO_DBG_ERROR, "SDIO: No irq in platform data\n");
+		return -EINVAL;
+	}
+
+	ret = devm_request_irq(dev, irq, handler, 0, "xradio", func);
+	if (ret) {
+		xr_printk(XRADIO_DBG_ERROR, "SDIO: Failed to request irq_wakeup.\n");
+		return -EINVAL;
+	}
+
+	return ret;
+}
+
+void xradio_free_gpio_irq(struct sdio_func *func)
+{
+	struct device *dev = &func->dev;
+
+	int irq = sunxi_wlan_get_oob_irq();
+	if (!irq) {
+		return;
+	}
+
+	disable_irq(irq);
+	devm_free_irq(dev, irq, func);
+}
